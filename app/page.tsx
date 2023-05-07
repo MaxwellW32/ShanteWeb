7d9c7ad6f2ef2d6ae07ca61684da29f45015c077
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-05-06 20:11:47 -0500

    image fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <>
-      <Image className={styles.dress} alt="dress" fill={true} src="/../public/dress.png"/>
+      <Image className={styles.dress} alt="dress" fill={true} src={require('../public/dress.png')}/>
       <h1>Congrats Shante!</h1>
       <p>Time to build a fashion website!</p>
-      <Image className={styles.thumbs} width={100} height={100} alt="thumbsUp" src="/../public/thumbs.jpg"/>
+      <Image className={styles.thumbs} width={100} height={100} alt="thumbsUp"src={require('../public/thumbs.jpg')}  />
       <p>Think of all the crochet!</p>
       
     </>
