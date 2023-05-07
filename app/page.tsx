37d4724f80472d50453bd799e5cdbe2df35e2c87
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-05-06 20:08:45 -0500

    yee

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <>
+      <Image className={styles.dress} alt="dress" fill={true} src="/../public/dress.png"/>
       <h1>Congrats Shante!</h1>
-      <p>Website time!</p>
+      <p>Time to build a fashion website!</p>
+      <Image className={styles.thumbs} width={100} height={100} alt="thumbsUp" src="/../public/thumbs.jpg"/>
+      <p>Think of all the crochet!</p>
+      
     </>
   )
 }
