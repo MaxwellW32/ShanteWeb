933107e2cbed4926c8bbe1e3cb0607d21b1813a8
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-05-13 21:44:53 -0500

    audio

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,91 @@
+"use client"
+
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect, useState } from 'react'
+
+import ReactPlayer from "react-player";
+
 
 export default function Home() {
+
+  const [name, setName] = useState({
+    firstName: "Shante",
+    lastName: "Wedderburn"
+  })
+
+  const [joke, setJoke] = useState("")
+
+  async function getNewJoke(firstName: string, lastName: string){
+
+    return ""
+
+
+    const response = await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`)
+    const jsonResponse = await response.json()
+
+    handleJokeSet(jsonResponse.value.joke)
+  }
+
+  function handleJokeSet(joke: string){
+    setJoke(joke)
+  }
+
+  function handleNameChange(option: string, value: string){
+
+    if (option === "firstName"){
+      setName(prevNameObj => {
+        return {...prevNameObj, firstName: value}
+      })
+    }else if(option === "lastName"){
+      setName(prevNameObj => {
+        return {...prevNameObj, lastName: value}
+      })
+    }
+
+  }
+
+  useEffect(()=>{
+    getNewJoke(name.firstName, name.lastName)
+  },[])
+
+  useEffect(()=>{
+
+    setTimeout(()=>{
+      console.log(`running now`)
+
+      // while (true) {
+      // }
+
+
+    },5000)
+  },[])
+
   return (
     <>
-      <Image className={styles.dress} alt="dress" fill={true} src={require('../public/dress.png')}/>
-      <h1>Congrats Shante!</h1>
-      <p>Time to build a fashion website!</p>
-      <Image className={styles.thumbs} width={100} height={100} alt="thumbsUp"src={require('../public/thumbs.jpg')}  />
-      <p>Think of all the crochet!</p>
+
+      <ReactPlayer url="/audio1.mp3" playing={false} />
       
+      {joke}
+
+      <div className={styles.jokeDiv}>
+        <button onClick={()=>{getNewJoke(name.firstName, name.lastName)}}>
+          See another joke
+        </button>
+
+        <p>Set names</p>
+
+        <input type='text' id='firsNameInput' onChange={(e)=>{
+          handleNameChange("firstName", e.target.value)
+        }}/>
+
+        <input type='text' id='lastNameInput' onChange={(e)=>{
+          handleNameChange("lastName", e.target.value)
+        }}/>
+
+
+      </div>
     </>
   )
 }
